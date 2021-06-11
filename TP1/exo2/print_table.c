d41504ee1ba79723ec713d99d5645244ad058763
Baladi_Imane <72796883+Imane-code-ux@users.noreply.github.com>
2021-06-11 09:53:55 +0100

    Update print_table.c

--- TP1/exo2/print_table.c
+++ TP1/exo2/print_table.c
@@ -1,1 +1,15 @@
 
+#include "header.h"
+
+/*
+** Note: iterating pointer array is the same as stack array.
+*/
+void    print_table(int *tab, int size)
+{
+    printf("Here is the array:\n");
+    for ( int i = 0; i < size; i++)
+    {
+        printf("%d ",tab[i]);
+    }
+    return ;
+}
