5ba24beb7a8ee2201da27960effc6c13c12e7a2f
Baladi_Imane <72796883+Imane-code-ux@users.noreply.github.com>
2021-06-11 12:41:29 +0100

    Update main.c

--- TP2/EXO1/main.c
+++ TP2/EXO1/main.c
@@ -1,1 +1,12 @@
+int main()
+	{
+		t_point *dim = allocateStruct();
+
+	//printf("enter two integers");
+	//scanf("%d%d", &(dim.a), &(dim.b));
+	printf("%d%d",dim->a,dim->b);
+	allocation(t_point dim);
+
+
+return 0;
 
