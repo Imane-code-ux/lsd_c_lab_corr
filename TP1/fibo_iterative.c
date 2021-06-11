3780e4675643530c2c4c4ced00713838f2144a9a
Baladi_Imane <72796883+Imane-code-ux@users.noreply.github.com>
2021-06-11 09:38:17 +0100

    Update fibo_iterative.c

--- TP1/fibo_iterative.c
+++ TP1/fibo_iterative.c
@@ -1,1 +1,13 @@
+#include "header.h"
+
+int iterative_fibo(int n)
+{
+    int f1=0,f2=1,s=0;
+        for(int i=1;i<n;i++){
+            s=f1+f2;
+            f1=f2;
+            f2=s;
+        }
+    return s;
+}
 
