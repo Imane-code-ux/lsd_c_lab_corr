2116b5463f23876c1331fe0df1983a5f7cb93460
Baladi_Imane <72796883+Imane-code-ux@users.noreply.github.com>
2021-06-11 10:00:19 +0100

    Update primes_opt.c

--- TP1/exo3/primes_opt.c
+++ TP1/exo3/primes_opt.c
@@ -1,1 +1,33 @@
+#include "header.h"
 
+// ? Note: A static function in C is a function that has a scope that is
+// limited to its object file. This means that the static function is only
+// visible in its object file.
+// A function can be declared as static function by placing the static keyword
+// before the function name
+
+static bool    is_prime_opt(int n)
+{
+    int i=2;
+    while (i<=sqrt((double)n))
+    {
+        if(n%i==0){
+            return false;
+        }
+        i++;
+        
+    }
+    return true;
+}
+
+
+void    get_primes_opt(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if(is_prime_opt(i)==true){
+            printf("%d\n",i);
+        }
+    }
+    return;
+}
