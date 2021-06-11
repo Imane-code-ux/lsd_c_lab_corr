48b44a99723657633d6887bc84ac5778c678d1ad
Baladi_Imane <72796883+Imane-code-ux@users.noreply.github.com>
2021-06-11 09:54:51 +0100

    Update rand_fill_table.c

--- TP1/exo2/rand_fill_table.c
+++ TP1/exo2/rand_fill_table.c
@@ -1,1 +1,16 @@
+#include "header.h"
 
+/*
+** Hint: You need to use rand function for generating random numbers
+** and to get out of getting the same series of random numbers every time
+** you run the program you need to use srand.
+** https://www.geeksforgeeks.org/rand-and-srand-in-ccpp/
+*/
+void rand_fill_table(int *tab, int size)
+{
+    srand(time(0));
+    for(int i=0;i<size;i++){
+        tab[i]=rand();
+    }
+    return ;
+}
