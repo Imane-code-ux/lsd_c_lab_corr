98be4760f87de9f17f124eff54a2326f906ce8b4
Baladi_Imane <72796883+Imane-code-ux@users.noreply.github.com>
2021-06-11 12:38:26 +0100

    Add files via upload

---
+++ TP2/EXO1/floodfile.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct s_point
+{
+	int a;
+	int b;
+} t_point;
+	
+
+t_point *allocateStruct()
+{
+	t_point *p = (t_point *)malloc(sizeof(t_point));
+	scanf("%d%d",&p->a,&p->b);
+	return p;
+
+}
+char**  allocation(t_point dim)
+{ 
+	char **p;
+	p=(int**)malloc(sizeof(char*)*dim.a);
+	for(int i =0;i<=dim.a;i++)
+		p[i]=(int*)malloc(sizeof(char)*dim.b);
+	for (int i=0;i<=dim.a;i++)
+		for(int j=0;j<=dim.b;j++){
+			scanf("%c",&(p[i][j]));}
+
+	return p
+
+}
+
+t_point  get_position(t_point pst)
+{
+	printf("entrer une position pour lançer le floodfile:");
+	scanf("%d",&x,&y);
+	pst.a=x;
+	pst.b=y;
+	return pst;
+}
+
+
+void floodfile(char ** Tab;t_point pst,t_point dim).
+{
+	 char c =Tab[pst.a][pst.b];
+	 Tab[pst.a][pst.b]=F;
+	 
+	 if(Tab[pst.x][pst.y+1]==c && y+1<b);
+		P.x=pst.x;
+		p.y=pst.y;
+
+
+			
+	
+
+Q
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	int main()
+	{
+		t_point *dim = allocateStruct();
+
+	//printf("enter two integers");
+	//scanf("%d%d", &(dim.a), &(dim.b));
+	printf("%d%d",dim->a,dim->b);
+	allocation(t_point dim);
+
+
+return 0;
+
+
+
+
+
+
