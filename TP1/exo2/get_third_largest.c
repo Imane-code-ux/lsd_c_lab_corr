d96e5a7e3b19685bc2d0c6b41f00bac433a471ba
Baladi_Imane <72796883+Imane-code-ux@users.noreply.github.com>
2021-06-11 09:52:49 +0100

    Update get_third_largest.c

--- TP1/exo2/get_third_largest.c
+++ TP1/exo2/get_third_largest.c
@@ -1,1 +1,27 @@
+#include "header.h"
+
+/*
+** Hint: for finding the largest third element in the array you don't need
+** to sort the whole array, only three variables is needed for this purpose
+** and the algorithm comlexity will be O(n).
+*/
+int     get_third_largest(int *tab, int size)
+{
+    int p=0,q=0,r=0;
+    for(int i=0;i<size;i++){
+        if (tab[i]>p){
+            r=q;
+            q=p;
+            p=tab[i];
+        }
+        if (tab[i]>q && tab[i]<p){
+            r=q;
+            q=tab[i];
+        }
+        if (tab[i]>r && tab[i]<q){
+            r=tab[i];
+        }
+    }
+    return r;
+}
 
